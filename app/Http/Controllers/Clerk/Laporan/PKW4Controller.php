645567f74ce0f7479bcf6ad3e9c99b1e3a3e1c45
Suhairi Abdul Hamid <suhairi81@gmail.com>
2015-05-18 15:55:41 +0800

    18-05-2015 4

--- app/Http/Controllers/Clerk/Laporan/PKW4Controller.php
+++ app/Http/Controllers/Clerk/Laporan/PKW4Controller.php
@@ -8,6 +8,8 @@ use App\Cases;
 use App\Profile;
 use App\Officer;
 use App\Remitance;
+use App\Mahkamah;
+use App\Kehadiran;
 
 //use Illuminate\Http\Request;
 
@@ -54,6 +56,8 @@ class PKW4Controller extends Controller {
         $cases          = Cases::where('caseNo', \Session::get('caseNo'))->first();
         $officer        = Officer::find($cases->officer);
         $remitance      = Remitance::where('caseNo', \Session::get('caseNo'))->first();
+        $mahkamah       = Mahkamah::find($cases->mahkamah);
+        $kehadiran      = Kehadiran::find($cases->kehadiran);
 
         // ###############      Settings      #############
 
@@ -64,114 +68,114 @@ class PKW4Controller extends Controller {
         $pdf->SetMargins(10, 10, 7);
         $pdf->SetAutoPageBreak('on', -7);
 
-//        // Halaman 1
-//
-//        $pdf->AddPage();
-//
-//        $pdf->SetFont('Arial', '', 8);
-//        $pdf->SetXY(10, 10);
-//        $pdf->Cell(195, 5, 'PKW Format 4', 0, 1, 'R');
-//        $pdf->Ln(5);
-//
-//
-//        // ###############     Row 1    ######################
-//
-//        $pdf->SetFont('Arial', 'B', 14);
-//        $pdf->Cell(195, 8, 'BORANG AKU JANJI', 0, 1, 'C');
-//        $pdf->SetFont('Arial', 'B', 14);
-//        $pdf->Cell(195, 8, 'PERINTAH KEHADIRAN WAJIB (PKW)', 0, 1, 'C');
-//        $pdf->Ln(5);
-//
-//        // ###############     Section 1    ######################
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(15, 8, 'Saya   ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(185, 8, $profile->nama, 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(45, 8, 'No. Kad Pengenalan :    ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(185, 8, $profile->noKP, 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(100, 8, 'diletakkan di bawah Perintah Kehadiran Wajib oleh ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(185, 8, '.....................', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(15, 8, 'selama  ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(30, 8, '.................', 0, 0, 'L');
-//        $pdf->Cell(30, 8, 'bermula  ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(30, 8, '.................', 0, 0, 'L');
-//        $pdf->Cell(15, 8, 'hingga  ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(30, 8, '.................', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(10, 8, 'di ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(185, 8, '.....................', 0, 1, 'L');
-//        $pdf->Ln(5);
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(100, 8, 'akan akur dengan syarat-syarat berikut :- ', 0, 0, 'L');
-//        $pdf->SetFont('Arial', 'U', 12);
-//        $pdf->Cell(185, 8, '.....................', 0, 1, 'L');
-//        $pdf->Ln(5);
-//        $pdf->Ln(5);
-//
-//
-//        // ###############     Section 2    ######################
-//
-//        $pdf->SetFont('Arial', 'B', 14);
-//        $pdf->Cell(195, 5, 'SYARAT-SYARAT PKW', 0, 1, 'C');
-//        $pdf->Ln(5);
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(7, 8, 'i. ', 0, 0, 'L');
-//        $pdf->Cell(100, 8, 'Mematuhi tempoh PKW seperti yang telah ditetapkan oleh perintah mahkamah.', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(7, 8, 'ii. ', 0, 0, 'L');
-//        $pdf->Cell(100, 8, 'Mematuhi setiap peraturan yang ditetapkan oleh Pusat Kehadiran Wajib.', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(7, 8, 'iii. ', 0, 0, 'L');
-//        $pdf->Cell(100, 8, 'Menghadirkan diri di PKW pada tarikh dan waktu yang telah ditetapkan oleh ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'Menghadirkan diri di PKW pada tarikh dan waktu yang telah ditetapkan oleh ', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(7, 8, 'iv. ', 0, 0, 'L');
-//        $pdf->Cell(100, 8, 'Sekiranya gagal menjalani kerja wajib kerana masalah kesihatan, saya mestilah ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'menghubungi Penyelia terlebih dahulu dan mengemukakan surat perubatan atau ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'sijil sakit daripada Pegawai Perubatan Kerajaan.', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(7, 8, 'v. ', 0, 0, 'L');
-//        $pdf->Cell(100, 8, 'Mengikuti arahan dan melaksanakan kerja wajib yang diberikan kepada saya oleh  ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'penyelia. ', 0, 1, 'L');
-//        $pdf->Ln(5);
-//
-//        $pdf->SetFont('Arial', '', 12);
-//        $pdf->Cell(7, 8, 'vi. ', 0, 0, 'L');
-//        $pdf->Cell(100, 8, 'Menghindarkan diri daripada melakukan apa sahaja aktiviti yang tidak bermoral,  ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'jenayah dan perkara yang bertentangan dengan undang-undang semasa serta ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'peraturan PKW semasa menjalankan kerja wajib; saya akan dipantau dan diselia  ', 0, 2, 'L');
-//        $pdf->Cell(100, 8, 'oleh Pegawai Pusat Kehadiran Wajib, Penyelia dan Pegawai Penjara yang dilantik.', 0, 1, 'L');
-//        $pdf->Ln(5);
+        // Halaman 1
+
+        $pdf->AddPage();
+
+        $pdf->SetFont('Arial', '', 8);
+        $pdf->SetXY(10, 10);
+        $pdf->Cell(195, 5, 'PKW Format 4', 0, 1, 'R');
+        $pdf->Ln(5);
+
+
+        // ###############     Row 1    ######################
+
+        $pdf->SetFont('Arial', 'B', 14);
+        $pdf->Cell(195, 8, 'BORANG AKU JANJI', 0, 1, 'C');
+        $pdf->SetFont('Arial', 'B', 14);
+        $pdf->Cell(195, 8, 'PERINTAH KEHADIRAN WAJIB (PKW)', 0, 1, 'C');
+        $pdf->Ln(5);
+
+        // ###############     Section 1    ######################
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(15, 8, 'Saya   ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(185, 8, $profile->nama, 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(45, 8, 'No. Kad Pengenalan :    ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(185, 8, $profile->noKP, 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(100, 8, 'diletakkan di bawah Perintah Kehadiran Wajib oleh ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(185, 8, $mahkamah->name, 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(15, 8, 'selama  ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(50, 8, $cases->hukuman, 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(18, 8, 'bermula  ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(30, 8, $this->tarikhReFormat($remitance->tarikhHukum), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(15, 8, 'hingga  ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(30, 8, $this->tarikhReFormat($remitance->tarikhAwal), 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(10, 8, 'di ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'U', 12);
+        $pdf->Cell(185, 8, $kehadiran->desc, 0, 1, 'L');
+        $pdf->Ln(5);
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(100, 8, 'akan akur dengan syarat-syarat berikut :- ', 0, 0, 'L');
+        $pdf->Ln(5);
+        $pdf->Ln(5);
+
+
+        // ###############     Section 2    ######################
+
+        $pdf->SetFont('Arial', 'B', 14);
+        $pdf->Cell(195, 5, 'SYARAT-SYARAT PKW', 0, 1, 'C');
+        $pdf->Ln(5);
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(7, 8, 'i. ', 0, 0, 'L');
+        $pdf->Cell(100, 8, 'Mematuhi tempoh PKW seperti yang telah ditetapkan oleh perintah mahkamah.', 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(7, 8, 'ii. ', 0, 0, 'L');
+        $pdf->Cell(100, 8, 'Mematuhi setiap peraturan yang ditetapkan oleh Pusat Kehadiran Wajib.', 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(7, 8, 'iii. ', 0, 0, 'L');
+        $pdf->Cell(100, 8, 'Menghadirkan diri di PKW pada tarikh dan waktu yang telah ditetapkan oleh ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'Menghadirkan diri di PKW pada tarikh dan waktu yang telah ditetapkan oleh ', 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(7, 8, 'iv. ', 0, 0, 'L');
+        $pdf->Cell(100, 8, 'Sekiranya gagal menjalani kerja wajib kerana masalah kesihatan, saya mestilah ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'menghubungi Penyelia terlebih dahulu dan mengemukakan surat perubatan atau ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'sijil sakit daripada Pegawai Perubatan Kerajaan.', 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(7, 8, 'v. ', 0, 0, 'L');
+        $pdf->Cell(100, 8, 'Mengikuti arahan dan melaksanakan kerja wajib yang diberikan kepada saya oleh  ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'penyelia. ', 0, 1, 'L');
+        $pdf->Ln(5);
+
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(7, 8, 'vi. ', 0, 0, 'L');
+        $pdf->Cell(100, 8, 'Menghindarkan diri daripada melakukan apa sahaja aktiviti yang tidak bermoral,  ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'jenayah dan perkara yang bertentangan dengan undang-undang semasa serta ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'peraturan PKW semasa menjalankan kerja wajib; saya akan dipantau dan diselia  ', 0, 2, 'L');
+        $pdf->Cell(100, 8, 'oleh Pegawai Pusat Kehadiran Wajib, Penyelia dan Pegawai Penjara yang dilantik.', 0, 1, 'L');
+        $pdf->Ln(5);
 
 
         // Halaman 2
@@ -293,7 +297,7 @@ class PKW4Controller extends Controller {
         $pdf->SetXY(123, 243);
         $pdf->Cell(32, 5, 'Nama Pegawai', 0, 2, 'L');
         $pdf->Cell(32, 5, 'PKW/Penyelia : ', 0, 2, 'L');
-        $pdf->Ln(7);
+        $pdf->Ln(9);
 
         $officerName = $this->strTrim($officer->name, 15);
         $posX = 155;
@@ -307,8 +311,6 @@ class PKW4Controller extends Controller {
             $posY += 5;
         }
 
-
-
         $pdf->SetXY(27, 253);
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(18, 5, 'No. KP :  ', 0, 0, 'L');
@@ -320,7 +322,7 @@ class PKW4Controller extends Controller {
         $pdf->Cell(18, 5, 'No. KP : ', 0, 0, 'L');
         $pdf->SetFont('Arial', 'U', 12);
         $pdf->Cell(30, 5, $officer->noKP, 0, 2, 'L');
-        $pdf->Ln(7);
+        $pdf->Ln(9);
 
         $pdf->SetXY(27, 258);
         $pdf->SetFont('Arial', '', 12);
@@ -333,7 +335,7 @@ class PKW4Controller extends Controller {
         $pdf->Cell(15, 5, 'Tarikh : ', 0, 0, 'L');
         $pdf->SetFont('Arial', 'U', 12);
         $pdf->Cell(30, 5, $this->tarikhReFormat($remitance->tarikhHukum), 0, 2, 'L');
-        $pdf->Ln(7);
+        $pdf->Ln(9);
 
 
         // ################   OUTPUT #####################
